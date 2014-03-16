9c9ba2d8048f719a84da53d79f1d5ccca8ecb0ee
baivong <baivong@users.noreply.github.com>
2014-03-17 04:06:35 +0700

    Update replacetext.js

--- source/replacetext.js
+++ source/replacetext.js
@@ -1,14 +1,13 @@
 /* jQuery plugin replaceText v0.1 <http://devs.forumvi.com/>	Copyright (c) 2014+ zzbaivong */
 (function ($) {
-    $.fn.replaceText = function (options) {
-        var settings = $.extend({}, options);
-        return this.each(function (index, obj) {
-            $(this).html(function () {
-                return this.innerHTML.replace(new RegExp(Object.keys(settings).join("|"), "gi"), function (txt) {
-                    txt = txt.replace(/(\"|\'|\(|\)|\[|\]|\.|\*|\!|\?|\$|\||\+|\^)/g, "\\$1");
-                    return settings[txt];
-                });
-            });
-        });
-    };
+	$.fn.replaceText = function (options) {
+		var settings = $.extend({}, options);
+		return this.each(function (index, obj) {
+			$(this).html(function () {
+				return this.innerHTML.replace(new RegExp(Object.keys(settings).toString().replace(/(\"|\'|\(|\)|\[|\]|\.|\*|\!|\?|\$|\||\+|\^)/g, "\\$1").replace(/,/g, "|"), "gi"), function (txt) {
+					return settings[txt];
+				});
+			});
+		});
+	};
 }(jQuery));
