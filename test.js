1915400f9cc8d98aa04d518d2d26ff800094ae30
baivong <baivong@users.noreply.github.com>
2014-03-17 00:42:53 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+// <p>CSS</p>
+// <p>Topic(s) being watched</p>
+// <p>http://devs.cf</p>
+$("p").replacetext({
+	"CSS": "<strong>CSS</strong>",
+	"Topic\\(s\\) being watched": "Chủ đề đang theo dõi",
+	"devs\\.cf": "devs.forumvi.com"
+});
