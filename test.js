708b36a79f35c2172f4ef5dcc8fea2cfa1e49559
baivong <baivong@users.noreply.github.com>
2014-03-17 03:09:58 +0700

    Update and rename test.js to demo.html

--- test.js
+++
@@ -1,8 +0,0 @@
-// <p>CSS</p>
-// <p>Topic(s) being watched</p>
-// <p>http://devs.cf</p>
-$("p").replaceText({
-	"CSS": "<strong>CSS</strong>",
-	"Topic\\(s\\) being watched": "Chủ đề đang theo dõi",
-	"devs\\.cf": "devs.forumvi.com"
-});
