58c57bc5ecf9ac1223635d0ec11d752f6889c6a1
baivong <baivong@users.noreply.github.com>
2014-03-17 00:46:31 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // <p>CSS</p>
 // <p>Topic(s) being watched</p>
 // <p>http://devs.cf</p>
-$("p").replacetext({
+$("p").replaceText({
 	"CSS": "<strong>CSS</strong>",
 	"Topic\\(s\\) being watched": "Chủ đề đang theo dõi",
 	"devs\\.cf": "devs.forumvi.com"
